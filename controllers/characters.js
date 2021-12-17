6bc650ef4c51864d2de27ed36ee9c8b46521bb08
Angela Quach <angelayunquach@gmail.com>
2021-12-17 16:56:27 -0500

    search function

--- controllers/characters.js
+++ controllers/characters.js
@@ -1,14 +1,32 @@
 import { marvelApi } from '../config/api.js'
+import { Character } from '../models/character.js'
 
 function search(req, res) {
-  marvelApi.get(`http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=f5fd89757a7f10387ce423f3f28c64df&hash=aaef7f942c150ae767af53eb559c9708`)
-    .then(character => {
-      console.log("response", character)
-      res.json(character.data) 
-    })
+  marvelApi.get(`http://gateway.marvel.com/v1/public/characters?&ts=1&apikey=f5fd89757a7f10387ce423f3f28c64df&hash=aaef7f942c150ae767af53eb559c9708&name=${req.body.query}`)
+    .then(character => res.json(character.data))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+  })
+}
+
+function addToTeam(req, res) {
+  Character.create(req.body.query) // is this right?
+  .then(character => res.status(201).json(character))
+  req.body.added_by = req.user.profile
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+}
+
+function addToComic(req, res) {
+  
 }
 
 
 export {
-  search
+  search,
+  addToTeam,
+  addToComic,
 }
