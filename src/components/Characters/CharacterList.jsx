61cda8f1b21d8238967bede568f5c8ec557dd5cd
Aishaml1 <Aisha.lugg1@icloud.com>
2021-12-20 23:29:24 -0500

    comics functionality

--- src/components/Characters/CharacterList.jsx
+++ src/components/Characters/CharacterList.jsx
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getCharacter } from "../../services/characterService";
 import Form from "./Form"
 import { useNavigate } from "react-router-dom"
+import ComicCard from "../../Comics/ComicCard"
 
 function CharacterList() {
   const [chars, setChars] = useState();
+  const [addComics, setAddComics] = useState([]);
   const navigate = useNavigate()
+
 
   const handleSearch = async (name) => {
     const charData = await getCharacter(name)
-    navigate(`/character/${charData.id}`, {state: charData})
-
+    navigate(`/character/${charData.id}`, { state: charData })
   }
+
+
 
   return (
     <>
       <h1> Search Character Page </h1>
-      <Form handleSearch={handleSearch} character={chars} />      
+      <Form handleSearch={handleSearch} character={chars} />
+      {/* {addComics.map((comic)=>(
+        <ComicCard 
+        comic={comic}
+        key={comic.id}
+        />
+      ))} */}
     </>
+    
   )
 }
 
