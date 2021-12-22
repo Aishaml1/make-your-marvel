fd799bc9d7a4b19855e071deb783c38a256af971
Mandy_Mascetti <armascetti@gmail.com>
2021-12-22 10:15:28 -0500

    deleteCharacter

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 import CharCard from "./CharCard"
 import { getMyProfile } from "../../services/profileService";
-import UserCard  from "../../components/misc/UserCard"
-import QuoteCard from "./QuoteCard";
+import UserCard from "../../components/misc/UserCard"
+import * as characterService from '../../services/characterService'
+// import QuoteCard from "./QuoteCard";
 
 const MyProfile = () => {
   const [character1, setCharacter1] = useState()
   const [character2, setCharacter2] = useState()
   const [character3, setCharacter3] = useState()
   const [profileData, setProfileData] = useState()
-  
+
   const updateCharacter = (updatedChar, id) => {
 
     if (id === character1._id) {
@@ -18,11 +19,26 @@ const MyProfile = () => {
       setCharacter2(updatedChar)
     } else {
       setCharacter3(updatedChar)
-    }  
+    }
   }
-  
+
+
+  const handleDeleteCharacter = async (characterId) => {
+    await characterService.deleteCharacter(characterId)
+    if (characterId === character1._id) {
+      setCharacter1(character1.filter((characters1) => characters1._id !== characterId))
+    } else if (characterId === character2._id) {
+      setCharacter2(character2.filter((characters2) => characters2._id !== characterId))
+    } else {
+      setCharacter3(character3.filter((characters3) => characters3._id !== characterId))
+    }
+  }
+
+
+
+
   useEffect(() => {
-    const getProfile = async() => {
+    const getProfile = async () => {
       const data = await getMyProfile()
       console.log("this is data in profile", data)
       setCharacter1(data.team[0])
@@ -36,24 +52,26 @@ const MyProfile = () => {
 
   return (
     <>
-      { profileData && 
-      <UserCard profileData={profileData} />      
+      {profileData &&
+        <UserCard profileData={profileData} />
       }
 
-      { character1 &&
-      <CharCard character={character1} updateCharacter={updateCharacter} />
+      {character1 &&
+        <CharCard character={character1} updateCharacter={updateCharacter} handleDeleteCharacter={handleDeleteCharacter}/>
       }
 
-      { character2 &&
-      <CharCard character={character2} updateCharacter={updateCharacter} />
-      }
-      
-      { character3 &&
-      <CharCard character={character3} updateCharacter={updateCharacter} />
+      {character2 &&
+        <CharCard character={character2} updateCharacter={updateCharacter} handleDeleteCharacter={handleDeleteCharacter} />
       }
 
-    </>    
+      {character3 &&
+        <CharCard character={character3} updateCharacter={updateCharacter} handleDeleteCharacter={handleDeleteCharacter}/>
+      }
+
+    </>
   )
 }
 
 export default MyProfile
+
+
