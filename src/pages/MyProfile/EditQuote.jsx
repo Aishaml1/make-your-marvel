89d6d404be477a4f3fe0348b3b1f567e24a34c53
Aishaml1 <Aisha.lugg1@icloud.com>
2021-12-22 17:33:01 -0500

    renderComics

--- src/pages/MyProfile/EditQuote.jsx
+++ src/pages/MyProfile/EditQuote.jsx
@@ -3,31 +3,31 @@ import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { editQuote } from "../../services/characterService"
 
 function EditQuote(props) {
-  const navigate = useNavigate()
-  const location = useLocation()
+	const navigate = useNavigate()
+	const location = useLocation()
 
-  console.log("location.state", location.state)
-  const [formData, setFormData] = useState("")
+	console.log("location.state", location.state)
+	const [formData, setFormData] = useState("")
 
-  const handleChange = evt => {
+	const handleChange = evt => {
 		setFormData(evt.target.value)
 	}
 
-  const handleUpdateQuote = async(e) => {
-    e.preventDefault()
-    try {
-      const formatted = {
-        content: formData
-      }
-      await editQuote(location.state.characterId, location.state.quoteId, formatted) 
-      navigate("/profile")
-    } catch (error) {
-      throw error
-    }
-  }
+	const handleUpdateQuote = async (e) => {
+		e.preventDefault()
+		try {
+			const formatted = {
+				content: formData
+			}
+			await editQuote(location.state.characterId, location.state.quoteId, formatted)
+			navigate("/profile")
+		} catch (error) {
+			throw error
+		}
+	}
 
-  useEffect(() => {
-    console.log("this is firing off")
+	useEffect(() => {
+		console.log("this is firing off")
 		setFormData(location.state.content)
 	}, [])
 
@@ -35,22 +35,22 @@ function EditQuote(props) {
 		<>
 			<h1>Edit Quote</h1>
 			<form autoComplete="off" onSubmit={handleUpdateQuote}>
-					<input 
-						type="text"
-						className="form-control"
-						id="name-input"
-						name="name"
-            value={formData}
-						required
-            onChange={(e) => handleChange(e)}
-					/>
-					<button
-						type="submit"
-          
-					>
-						Update Quote
-					</button>
-					<Link to="/">Cancel</Link>
+				<input
+					type="text"
+					className="form-control"
+					id="name-input"
+					name="name"
+					value={formData}
+					required
+					onChange={(e) => handleChange(e)}
+				/>
+				<button
+					type="submit"
+
+				>
+					Update Quote
+				</button>
+				<Link to="/">Cancel</Link>
 			</form>
 		</>
 	)
